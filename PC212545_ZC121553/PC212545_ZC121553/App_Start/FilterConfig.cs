10f6d3cf75b4d66cbebaabc42315dc09ce239241
Miguelinix <73465109+Miguelinix@users.noreply.github.com>
2021-10-20 12:29:21 -0600

    Add files via upload

---
+++ PC212545_ZC121553/PC212545_ZC121553/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PC212545_ZC121553
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
